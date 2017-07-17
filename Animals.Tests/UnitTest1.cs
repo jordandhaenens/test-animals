eaa6459e6ebb6ed7628655e8ec0f22a87b1d3446
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-07-17 11:08:27 -0500

    initial commit

---
+++ Animals.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace Animals.Tests
+{
+    public class AnimalShould
+    {   
+
+        private string _species;
+        private double _walkingSpeed;
+        public string Name { get; }
+        
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
